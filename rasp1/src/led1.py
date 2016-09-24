746b7c427e7f2347412a046aa5e959e3b3634354
ligson <593949938@qq.com>
2016-09-24 14:02:36 +0800

    dd

--- rasp1/src/led1.py
+++ rasp1/src/led1.py
@@ -7,9 +7,14 @@ GPIO.setup(21, GPIO.OUT)
 
 if __name__ == '__main__':
     print("xxxxx")
-    while True:
-        print("---")
+    try:
+        while True:
+        print "---"
         GPIO.output(21, GPIO.HIGH)
         time.sleep(1)
         GPIO.output(21, GPIO.LOW)
             time.sleep(1)
+    except KeyboardInterrupt:
+        print "正常退出"
+        pass
+GPIO.cleanup()
