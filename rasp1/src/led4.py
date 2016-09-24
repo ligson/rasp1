be95d9b260b5b59f69695d3d2ea5af30bcf7ff8b
ligson <593949938@qq.com>
2016-09-24 15:16:06 +0800

    led4

--- rasp1/src/led4.py
+++ rasp1/src/led4.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 import time
+import random
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(19, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
 
-pwm1 = GPIO.PWM(13, 0)
-pwm2 = GPIO.PWM(19, 0)
-pwm3 = GPIO.PWM(26, 0)
+pwm1 = GPIO.PWM(13, 1)
+pwm2 = GPIO.PWM(19, 1)
+pwm3 = GPIO.PWM(26, 1)
 
 pwm1.start(0)
 pwm2.start(0)
@@ -22,7 +23,10 @@ def changeColor(r, g, b):
 if __name__ == '__main__':
     try:
         while True:
-            changeColor(255, 0, 0)
+            n1 = int(random.random()*100)
+            n2 = int(random.random()*100)
+            n3 = int(random.random()*100)
+            changeColor(n1,n2,n3)
             time.sleep(0.5)
     except KeyboardInterrupt:
         pass
