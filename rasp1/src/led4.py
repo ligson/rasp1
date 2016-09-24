f2967797c05a642df52f7f58aea837277a954c3d
ligson <593949938@qq.com>
2016-09-24 14:33:34 +0800

    4lid

---
+++ rasp1/src/led4.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+pwm = RPi.GPIO.PWM(21, 50)
+pwm.start(0)
+
+if __name__ == '__main__':
+    try:
+        while True:
+            for i in xrange(0, 100, 2):
+                pwm.ChangeDutyCycle(i)
+                time.sleep(0.01)
+            for i in xrange(100, 0, 2):
+                pwm.ChangeDutyCycle(i)
+                time.sleep(0.01)
+    except KeyboardInterrupt:
+        pass
+pwm.stop()
+GPIO.cleanup()
+
